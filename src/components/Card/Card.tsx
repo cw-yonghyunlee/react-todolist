0e5e04ec8ffbfbd1ce9ec9a1c39431cb841e0eaf
Yonghyun.Lee <yonghyun.lee@coway.com>
2022-01-06 18:51:54 +0900

    feat: sample Component 구현

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import List from '../List';
+
+function Card(): JSX.Element {
+	return (
+		<article>
+			<h1>To Do List</h1>
+			<List />
+		</article>
+	);
+}
+
+export default Card;
---
+++ src/components/List/List.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function List(): JSX.Element {
+	return (
+		<ul>
+			<li>test</li>
+			<li>test</li>
+			<li>test</li>
+		</ul>
+	);
+}
+
+export default List;
