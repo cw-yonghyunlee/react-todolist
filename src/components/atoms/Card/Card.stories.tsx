bbb2a80d76dd56c8e16d848eab1021acb8ab84e1
Yonghyun.Lee <yonghyun.lee@coway.com>
2022-01-14 17:02:31 +0900

    chore(#4): Storybook atoms component에 도입

---
+++ src/components/atoms/Card/Card.stories.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Card from './index';
+import { ComponentStory } from '@storybook/react';
+
+export default {
+  title: 'Atoms/Card',
+  component: Card,
+};
+
+const Template: ComponentStory<typeof Card> = args => <Card {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  children: '내용',
+};
---
+++ src/components/atoms/CheckBox/CheckBox.stories.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import CheckBox from './index';
+import { ComponentStory } from '@storybook/react';
+
+export default {
+  title: 'Atoms/CheckBox',
+  component: CheckBox,
+};
+
+const Template: ComponentStory<typeof CheckBox> = args => (
+  <CheckBox {...args} />
+);
+export const Default = Template.bind({});
