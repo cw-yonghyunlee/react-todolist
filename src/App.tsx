62de332bd7d8dd5bf081ec7cdf88ce07e8ae4c08
Yonghyun.Lee <yonghyun.lee@coway.com>
2022-01-18 09:28:05 +0900

    refactor(#1): context api 제거
    - 한 컴포넌트에서만 data를 사용하기 때문에 굳이 context api가 필요하지 않음.
    - localStorage에 data를 이미 저장하고 있어 굳이 필요하지 않음.
    - localStorage를 조회하는 쪽으로 구현 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import { ToDoListContainerProvider } from './contexts/ToDoListContainer';
 import ToDoListCard from './components/ToDo/ToDoListCard';
 
 function App(): JSX.Element {
   return (
-    <ToDoListContainerProvider>
+    <>
       <h1>To Do List</h1>
       <ToDoListCard />
-    </ToDoListContainerProvider>
+    </>
   );
 }
 
--- src/components/ToDo/ToDoListCard.tsx
+++ src/components/ToDo/ToDoListCard.tsx
@@ -1,62 +1,73 @@
-import React, { useContext } from 'react';
-import { ToDoListContainerContext } from '../../contexts/ToDoListContainer';
+import React, { useEffect, useState } from 'react';
 import List from '../List/List';
 import Card from '../atoms/Card';
 import AddToDoForm from './AddToDoForm';
 import { UseFormFieldValues } from '../../types/form';
+import {
+  getLocalStorage,
+  setLocalStorage,
+} from '../../utils/local-storage-manager';
+import { Work } from '../../types/work';
+
+const currentDate = new Date();
 
 function ToDoListCard(): JSX.Element {
-  const toDoList = useContext(ToDoListContainerContext);
+  const [list, setList] = useState<Work[]>(getLocalStorage().list);
+  const [lastId, setLastId] = useState<number>(getLocalStorage().lastId);
+
+  useEffect(() => {
+    setLocalStorage({
+      list,
+      lastId,
+    });
+  }, [list, lastId]);
 
   const changeWorkStatus = (id: number): void => {
-    const targetItem = toDoList.list.find(item => item.id === id);
+    const targetItem = list.find(item => item.id === id);
     if (!targetItem) {
       console.error('not found id');
       return;
     }
     targetItem.isCompleted = !targetItem.isCompleted;
-    toDoList.actions.setList([...toDoList.list]);
+    setList([...list]);
   };
 
   const deleteWork = (id: number): void => {
-    toDoList.actions.setList(toDoList.list.filter(item => item.id !== id));
+    setList(list.filter(item => item.id !== id));
   };
 
   const addWork = (values: UseFormFieldValues): void => {
     const newData = [
-      ...toDoList.list,
+      ...list,
       {
-        id: toDoList.lastId + 1,
+        id: lastId + 1,
         isCompleted: false,
         description: values['description']!,
         createdAt: new Date(),
         expiredAt: new Date(values['expiredDate']!),
       },
     ];
-    toDoList.actions.setList(newData);
-    toDoList.actions.setLastId(toDoList.lastId + 1);
+    setList(newData);
+    setLastId(lastId + 1);
   };
 
   const editWork = (id: number, values: UseFormFieldValues): void => {
-    const targetItem = toDoList.list.find(item => item.id === id);
+    const targetItem = list.find(item => item.id === id);
     if (!targetItem) {
       console.error('not found id');
       return;
     }
-
     targetItem.description = values['description']!;
     targetItem.expiredAt = new Date(values['expiredDate']!);
-    toDoList.actions.setList([...toDoList.list]);
+    setList([...list]);
   };
 
   return (
     <Card>
-      <h3>{new Date().toLocaleDateString()}</h3>
+      <h3>{currentDate.toLocaleDateString()}</h3>
       <List
-        list={toDoList.list
-          .filter(
-            item => item.expiredAt.getTime() > toDoList.currentDate.getTime(),
-          )
+        list={list
+          .filter(item => item.expiredAt.getTime() > currentDate.getTime())
           .map(item => ({
             id: item.id,
             title: item.description,
@@ -70,10 +81,8 @@ function ToDoListCard(): JSX.Element {
       <List
         title="기한 만료된 일"
         itemClassName="expired"
-        list={toDoList.list
-          .filter(
-            item => item.expiredAt.getTime() < toDoList.currentDate.getTime(),
-          )
+        list={list
+          .filter(item => item.expiredAt.getTime() < currentDate.getTime())
           .map(item => ({
             id: item.id,
             title: item.description,
